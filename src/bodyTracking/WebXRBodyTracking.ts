e1a3e5652bc4f594a30df2b0d6e79eb366c23416
drumath <2237k.drumath@gmail.com>
2024-06-02 05:02:47 +0900

    :sparkles: onframe以外の処理を落書きしてみる

--- src/bodyTracking/WebXRBodyTracking.ts
+++ src/bodyTracking/WebXRBodyTracking.ts
@@ -1,6 +1,56 @@
-import { WebXRAbstractFeature } from "@babylonjs/core";
+import {
+  Observable,
+  Quaternion,
+  Vector3,
+  WebXRAbstractFeature,
+  WebXRSessionManager,
+} from "@babylonjs/core";
+
+export interface IWebXRBodyJointPose {
+  position: Vector3;
+  rotation: Quaternion;
+}
+
+export interface IWebXRBpdyTrackingOptions {
+  baseXRSpace?: XRSpace;
+}
+
+export type WebXRBodyJointMap = Map<XRBodyJoint, IWebXRBodyJointPose>;
+export type ReadonlyWebXRBodyJointMap = ReadonlyMap<
+  XRBodyJoint,
+  IWebXRBodyJointPose
+>;
 
 export class WebXRBodyTracking extends WebXRAbstractFeature {
+  public onBodyTrackedObservable: Observable<ReadonlyWebXRBodyJointMap> =
+    new Observable();
+
+  private _joints?: WebXRBodyJointMap;
+
+  public get size(): number | undefined {
+    return this._joints?.size;
+  }
+
+  public get joints(): ReadonlyWebXRBodyJointMap | undefined {
+    return this._joints;
+  }
+
+  public constructor(
+    _xrSessionManager: WebXRSessionManager,
+    private _options: IWebXRBpdyTrackingOptions = {}
+  ) {
+    super(_xrSessionManager);
+    this.xrNativeFeatureName = "body-tracking";
+  }
+
+  public get baseSpace(): XRSpace | undefined {
+    return this._options.baseXRSpace;
+  }
+
+  public setBaseSpace(baseSpace: XRSpace): void {
+    this._options.baseXRSpace = baseSpace;
+  }
+
   protected _onXRFrame(_xrFrame: XRFrame): void {
     throw new Error("Method not implemented.");
   }
